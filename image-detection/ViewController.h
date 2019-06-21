ca3a48c3e309de3473b8081cd00ae00b5a06b387
Yerzhan Serikbay <yerzhan.tulkubayev@gmail.com>
2019-06-21 16:18:11 +0800

    Initial Commit

---
+++ image-detection/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  image-detection
+//
+//  Created by YS on 6/21/19.
+//  Copyright © 2019 YS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ image-detection/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  image-detection
+//
+//  Created by YS on 6/21/19.
+//  Copyright © 2019 YS. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
